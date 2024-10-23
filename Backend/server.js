4282a6697e44e8a054a3e6a43f0bf1327fc87ed1
manishpatel28598 <manishpatel.28598@gmail.com>
2024-10-23 13:45:43 +0530

    add build folder

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const cors = require('cors');
-const morgan = require('morgan')
-const connectDb = require('./config/db');
-const dotenv = require('dotenv');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDb = require("./config/db");
+const dotenv = require("dotenv");
+const path = require("path");
+const app = express();
 const PORT = process.env.PORT || 8000;
 
 //dotenv config
 dotenv.config();
 
 //mongodeb connection
-connectDb()
+connectDb();
 
 //middleware
 app.use(cors());
@@ -19,15 +20,20 @@ app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
-app.use('/api/v1/admin', require("./routes/adminRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+app.use(express.static(path.join(__dirname, "../Frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../Frontend/build/index.html"));
+});
 
 //simple Api route
 
-app.get('/api/data', (req, res)=>{
-    res.send({message: "hello from the backend"});
+app.get("/api/data", (req, res) => {
+  res.send({ message: "hello from the backend" });
 });
 
-app.listen(PORT, ()=>{
-    console.log(`server is running at port: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is running at port: ${PORT}`);
+});
